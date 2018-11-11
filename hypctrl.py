0dd2e298382c53a1bb216254750ec8b8c471b9ae
christof1977 <github@burningwheels.com>
2018-11-11 23:05:35 +0100

    hypctrl.py fertig gemachrt

--- hypctrl.py
+++ hypctrl.py
@@ -3,6 +3,7 @@
 import time
 import os
 import subprocess
+from subprocess import DEVNULL
 from libby.logger import logger
 
 import RPi.GPIO as GPIO
@@ -23,59 +24,61 @@ class Hypctrl():
         GPIO.output(self.Out_ext1, GPIO.LOW) # Switch amp power supply off
 
 
-    def setScene(self):
+    def setScene(self, color=None):
+        if(color is not None):
+            self.color=int(color)
+        else:
+            if(self.color == len(self.cList)-1):
+                self.color = 0
+            else:
+                self.color += 1
+        logger("Scene: " + self.cList[self.color])
         cmd = '/usr/bin/hyperion-remote'
         if self.color == 0:
             args = ['-c', 'black']
-            msg = "Off"
             GPIO.output(self.Out_ext1, GPIO.LOW)
-            logger("   Off", logging)
-            self.color += 1
         elif self.color == 1:
             args = ['-x']
-            msg = "Kodi"
             GPIO.output(self.Out_ext1, GPIO.LOW)
-            logger("   Kodi", logging)
-            self.color +=1
         elif self.color == 2:
             args = ['-x']
-            v4l_ret = subprocess.Popen([run_path+'/hyp-v4l.sh', '&'])
-            msg = "BluRay"
-            logger("    BluRay", logging)
+            cmd = '/usr/bin/hyperion-v4l2'
+            args = ['-d', '/dev/video0',
+                    '--input', '0',
+                    '-v', 'PAL',
+                    '--width', '240',
+                    '--height', '192',
+                    '--frame-decimator', '2',
+                    '--size-decimator', '4',
+                    '--red-threshold', '0.1',
+                    '--green-threshold', '0.1',
+                    '--blue-threshold', '0.1'
+                    ]
             GPIO.output(self.Out_ext1, GPIO.LOW)
-            self.color += 1
         elif self.color == 3:
             v4l_ret = subprocess.call(['/usr/bin/killall',  'hyperion-v4l2'])
             args = ['-c',  self.cList[self.color+1]]
             msg = "Farbe: "+ self.cList[self.color+1]+" und Schrank"
-            logger("   " + self.cList[self.color+1]+" und Schrank", logging)
             GPIO.output(self.Out_ext1, GPIO.HIGH)
-            self.color += 1
+            #self.color += 1
         elif self.color > 3:
             args = ['-c',  self.cList[self.color]]
             msg = "Farbe: " + self.cList[self.color]
-            logger("   " + self.cList[self.color], logging)
             GPIO.output(self.Out_ext1, GPIO.LOW)
-            if self.color == len(self.cList)-1:
-               self.color = 0
-            else:
-               self.color += 1
-        else:
-            self.color = 0
-            return()
-        #cmd = cmd+args
-        #hyp = subprocess.call(cmd)
-        #print(cmd, *args)
-        #for arg in args:
-        #            print("another arg through *argv :", arg)
-        hyp = subprocess.Popen([cmd, *args])
+        hyp = subprocess.Popen([cmd, *args], stdout=DEVNULL, stderr=DEVNULL)
+        #hyp = subprocess.Popen([cmd, *args])
         return()
+
+    def getScene(self):
+        return(self.cList[self.color])
 
 def main():
     hyp = Hypctrl()
+    #hyp.setScene(color=3)
     for i in range(7):
         hyp.setScene()
-        time.sleep(1)
+        print(hyp.getScene())
+        time.sleep(2)
 
 
 if(__name__ == "__main__"):
